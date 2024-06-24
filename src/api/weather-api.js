435a3a6aef1f15a13759345f932053ed0fb2b2eb
daehan <dhwon@hmit.co.kr>
2024-06-24 11:30:39 +0900

    exception api key

--- src/api/weather-api.js
+++ src/api/weather-api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const getWeather = () => {
   return axios({
     method: 'GET',
-    url: 'https://api.openweathermap.org/data/2.5/weather?id=1838519&units=metric&appid=c8621d9cff81136a5faf27497b250865',
+    url: `https://api.openweathermap.org/data/2.5/weather?${process.env.REACT_APP_WEATHER_ID}`,
   });
 };
 
